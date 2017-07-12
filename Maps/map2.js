cbe3c89c90ad5c526056e86ac13b4b0d45b30c0a
Didrik Kvanvik <didrik.kvanvik@gmail.com>
2017-07-12 14:54:35 +0200

    Reverted map changes

--- Maps/map2.js
+++ Maps/map2.js
@@ -21,6 +21,20 @@ var map2 = {
      */
 
 
+    keys: [
+        {id: 0, colour: '#333', solid: 0},
+        {id: 1, colour: '#888', solid: 0},
+        {id: 2, colour: '#555', solid: 1, bounce: 0.35},
+        {id: 3, colour: 'rgba(121, 220, 242, 0.4)', friction: {x: 0.9,y: 0.9}, gravity: {x: 0,y: 0.1}, jump: 1, fore: 1},
+        {id: 4, colour: '#777', jump: 1},
+        {id: 5, colour: '#E373FA', solid: 1, bounce: 1.1},
+        {id: 6, colour: '#666', solid: 1, bounce: 0},
+        {id: 7, colour: '#73C6FA', solid: 0, script: 'change_colour'},
+        {id: 8, colour: '#67e534', solid: 0, script: 'next_level'},
+        {id: 9, colour: '#C93232', solid: 0, script: 'death'},
+        {id: 10, colour: '#555', solid: 1},
+        {id: 11, colour: '#f1f218', solid: 0,script: 'unlock'}
+    ],
 
     /* An array representing the map tiles. Each number corresponds to a key */
     data: [
@@ -30,7 +44,7 @@ var map2 = {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 2, 2, 2],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 2, 2, 2],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 9, 10, 10, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 10, 2],
         [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 10, 2],
         [2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 10, 2],
         [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 10, 2],
@@ -68,6 +82,27 @@ var map2 = {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ],
 
+    /* Gravity */
+
+    gravity: {
+        x: 0,
+        y: 0.3
+    },
+
+    /* Velocity limits */
+
+    vel_limit: {
+        x: 2,
+        y: 16
+    },
+
+    /* Movement speeds */
+
+    movement_speed: {
+        jump: 6,
+        left: 0.3,
+        right: 0.3
+    },
 
     /* The coordinates at which the player spawns and the colour of the player */
 
@@ -75,6 +110,14 @@ var map2 = {
         x: 33,
         y: 0,
         colour: '#fff'
-    }
+    },
 
+    /* scripts referred to by the "script" variable in the tile keys */
+
+    scripts : {
+        change_colour: 'game.player.colour = "#"+(Math.random()*0xFFFFFF<<0).toString(16);',
+        next_level: 'alert("Yay! You made it through the map, onwards to the next!"); death_counter = 0; game.load_map(map3);',
+        death: '++death_counter; if(death_counter === 3) { alert("You died!"); death_counter = 0; game.load_map(map1);} else {game.load_map(map2);} document.getElementById("death_counter").innerHTML = death_counter;',
+        unlock: 'game.current_map.keys[10].solid = 0;game.current_map.keys[10].colour = "#888";'
+    }
 };
